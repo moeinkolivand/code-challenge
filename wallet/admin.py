a4c83d2b4ba0ee839b26e9c8de5559fc1de84985
moeinupset <moeinupset@gmail.com>
2023-09-10 23:39:50 +0330

    change field quantity to biginteger

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from wallet.models import Wallet,WalletCurrency
+from wallet.models import Wallet, WalletCurrency
 
 admin.site.register(Wallet)
 admin.site.register(WalletCurrency)
